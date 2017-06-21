b528dd98b3a17889641cce009eae46013b684430
jgwise <jgwise22@outlook.com>
2017-06-21 19:37:20 -0400

    Added Target with Health Drain

--- You Must Save Her Prototyping/Assets/BeamController.cs
+++ You Must Save Her Prototyping/Assets/BeamController.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class BeamController : MonoBehaviour {
 
 	public SpriteRenderer lightBeam; 
+	public BoxCollider2D beamCol;
+	public bool takeDamage;
 
 	void Start () {
 		lightBeam = GetComponent<SpriteRenderer> ();
+		beamCol = GetComponent<BoxCollider2D> ();
+		beamCol.enabled = false;
 		lightBeam.enabled = false;
+		takeDamage = false;
 	}
 
 
@@ -16,10 +21,27 @@ public class BeamController : MonoBehaviour {
 
 		//Light Aura
 		if (GameObject.Find ("Aura").GetComponent<AuraShowHide> ().lightAura.enabled == false) {
-			if (Input.GetKey (KeyCode.Mouse0))
+			if (Input.GetKey (KeyCode.Mouse0)) {
 				lightBeam.enabled = true;
-			else if (Input.GetKeyUp (KeyCode.Mouse0))
+				beamCol.enabled = true;
+			} else if (Input.GetKeyUp (KeyCode.Mouse0)) {
 				lightBeam.enabled = false;	
+				beamCol.enabled = true;
+			}
 		}
 	}
+		
+	void OnTriggerStay2D(Collider2D other) 
+	{
+		if (other.gameObject.CompareTag ("Enemy")) {
+			takeDamage = true;
+		} else
+			takeDamage = false;
+	}
+
+	void Update () {
+		if (takeDamage == true)
+		GameObject.Find ("myEnemy").GetComponent<EnemyHealth> ().health -= Time.deltaTime * 1; 
+	}
+		
 }
---
+++ You Must Save Her Prototyping/Assets/EnemyHealth.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour {
+	 
+	public double health;
+	public SpriteRenderer enemy;
+
+	// Use this for initialization
+	void Start () {
+		enemy = GetComponent<SpriteRenderer> ();
+		health = 1.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (health < 0.001f) {
+			this.gameObject.SetActive (false);
+		}
+		Debug.Log (health);
+	}
+}
+ 
